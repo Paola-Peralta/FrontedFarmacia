dc02a88bc9d534171a86d65217af5e485decc6eb
riclaj36 <richardalexanderriosgaitan@gmail.com>
2025-04-24 13:21:41 -0600

    Agregado modal con animación y botón de cierre en clientes

---
+++ pages/scriptPages/scriptsClientes.js
@@ -0,0 +1,30 @@
+function openModal() {
+    const modalContainer = document.querySelector('.modal-container');
+    modalContainer.style.display = 'flex'; // Mostrar el modal
+    setTimeout(() => {
+      modalContainer.classList.add('active');
+    }, 10); // pequeño delay para que la transición se aplique
+  }
+  
+function closeModal() {
+    const modalContainer = document.querySelector('.modal-container');
+    modalContainer.classList.remove('active');
+    setTimeout(() => {
+      modalContainer.style.display = 'none';
+    }, 300); // espera que termine la animación de opacidad
+  }
+
+  // Cerrar modal al hacer clic fuera del contenido
+window.addEventListener('click', function(e) {
+    const modalContainer = document.querySelector('.modal-container');
+    if (e.target === modalContainer) {
+      closeModal();
+    }
+  });
+  
+  // También cerrar al presionar Escape 
+    window.addEventListener('keydown', function(e) {
+        if (e.key === 'Escape') {
+          closeModal();
+        }
+      });
